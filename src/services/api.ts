fd662eecc064aa6abcfbc9fe9f3b4366fe886fd5
victhorhugoo <victhorhugo.dev@gmail.com>
2026-01-12 23:17:20 -0300

    chore: fix api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "patos-burguers-backend-gtdn489ct-victors-projects-b875633f.vercel.app",
+    "https://patos-burguers-backend-gtdn489ct-victors-projects-b875633f.vercel.app",
 });
 
 export default api;
