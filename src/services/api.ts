c9a1004dbc0c26b69a4adc6b73d26ee4bf05e3bd
victhorhugoo <victhorhugo.dev@gmail.com>
2026-01-12 22:25:08 -0300

    feat: deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4444'
-})
+  baseURL:
+    "patos-burguers-backend-gtdn489ct-victors-projects-b875633f.vercel.app",
+});
 
 export default api;
